a7c1ec66bbd02d3000c82551484795aad61a5e8a
oliver butterley <oliver.butterley@gmail.com>
2022-09-27 12:27:36 +0200

    🐛 fixed

--- typeset.sh
+++ typeset.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+echo 'Typesetting graphics...'
 latexmk -pdf -shell-escape -cd --output-directory=build graphics/*.tex
+echo 'Removing aux files related to graphics'
 latexmk -pdf -c -cd --output-directory=build graphics/*.tex
+echo 'Typesetting main text...'
+mkdir --parents build/chapters build/frontmatter
 latexmk -pdf --output-directory=build main.tex
+echo 'removing aux files related to main text...'
 latexmk -pdf -c --output-directory=build main.tex
+echo 'Moving final typeset document and cleaning up...'
 mkdir dist
 mv build/main.pdf dist/main.pdf
+rmdir --parents build/chapters build/frontmatter build
